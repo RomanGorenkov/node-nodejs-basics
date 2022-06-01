57801b8cf6d48b5dc5e40721c8e50e02e4fb01bf
RomanGorenkov812 <roman.gorenkov@tobania.be>
2022-06-01 20:43:24 +0300

    [TASK-1] Hash
    Work done:
    * Hash module

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,19 @@
+import crypto from 'crypto';
+import * as fsPromises from 'fs/promises';
+import { fileURLToPath } from "node:url";
+import { dirname } from "path";
+
 export const calculateHash = async () => {
-    // Write your code here 
+    const filename = fileURLToPath(import.meta.url);
+    const folderName = dirname(filename);
+    const filePath = `${folderName}/files/fileToCalculateHashFor.txt`;
+
+    fsPromises.readFile(filePath).then(fileBuffer => {
+        const hashSum = crypto.createHash('sha256');
+
+        hashSum.update(fileBuffer);
+        console.log(hashSum.digest('hex'));
+    });
 };
+
+calculateHash();
