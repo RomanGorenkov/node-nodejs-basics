058b77d27300998a259adc2d1afa32d742dba5a2
RomanGorenkov812 <roman.gorenkov@tobania.be>
2022-06-01 18:16:34 +0300

    [TASK-1] CLI
    Work done:
    * CLI module

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,12 @@
+const getCustomArguments = () => process.argv.slice(2);
+
 export const parseArgs = () => {
-    // Write your code here 
+    const customArguments = getCustomArguments();
+    const keys = customArguments.filter((_, index) => index % 2 === 0);
+    const values = customArguments.filter((_, index) => index % 2 !== 0);
+    const formatArgStrings = keys.map((key, index) => `${key} is ${values[index]},`);
+
+    console.log(formatArgStrings.join(' '));
 };
+
+parseArgs();
