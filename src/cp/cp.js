4c8752a310c600b67d937081a5c648093c94c95e
RomanGorenkov812 <roman.gorenkov@tobania.be>
2022-06-05 23:40:08 +0300

    [TASK-1] ChildProcess
    Work done:
    * ChildProcess module

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,10 @@
+import cp from 'node:child_process'
+import fs from 'fs'
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+    const spawnProcess = cp.fork('./files/script.js', args)
+    const write = fs.createWriteStream
 };
+
+spawnChildProcess(['lol', 'kek'])
+    .then(() => 'Successfully started child process')
