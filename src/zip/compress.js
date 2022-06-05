84d7895c858a023f4e0fcf17a2a1973eb551d7c2
RomanGorenkov812 <roman.gorenkov@tobania.be>
2022-06-05 21:49:12 +0300

    [TASK-1] Zlib
    Work done:
    * Zlib module

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,13 @@
+import fs from 'fs';
+import zlib from 'zlib';
+
 export const compress = async () => {
-    // Write your code here 
+    const zip = zlib.createGzip();
+    const readStream = fs.createReadStream('./files/fileToCompress.txt');
+    const writeStream = fs.createWriteStream('./files/archive.gz');
+
+    readStream.pipe(zip).pipe(writeStream);
+    console.log("zipped");
 };
+
+compress();
